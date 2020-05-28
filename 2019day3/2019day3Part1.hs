3443a982287ff0b0a49025e22a9b38b574f35935
MatthewChen37 <epicninjasauce@gmail.com>
2020-05-28 16:36:14 -0400

    :q

--- 2019day3/2019day3Part1.hs
+++ 2019day3/2019day3Part1.hs
@@ -21,9 +21,6 @@ generate_path path =
     --list of points is of type [[(Int, Int)]]
     --TODO: break down the lists of lists into a continuous list
 
-
-
-
 generate_points :: (Int,Int) -> [String] -> [(Int, Int)] -> [(Int,Int)]
 generate_points input list_of_points
     |direction == 'U' = go_up input list_of_points
@@ -33,22 +30,18 @@ generate_points input list_of_points
     |direction == '\n' = list_of_points
     where direction = head (input !! 0)
 
-
-
-go_up :: [String] -> [(Int, Int)]
 go_up string_path list_of_points =
     let parsed_int = parse_current string_path
-    tuple_up parsed_int list_of_points
+    in tuple_up parsed_int list_of_points
 
 tuple_up :: Int -> [String] -> [(Int, Int)]
 
 tuple_up 0 list_of_points = list_of_points
 tuple_up increment list_of_points =
-
     let last_point = tail list_of_points
-    let new_point = ((fst last_point), (snd last_point) + 1)
-    let new_list_of_points = list_of_points ++ new_point
-    tuple_up (increment - 1) new_list_of_points
+        new_point = ((fst last_point), (snd last_point) + 1)
+        new_list_of_points = list_of_points ++ new_point
+    in tuple_up (increment - 1) new_list_of_points
 
 go_down :: [String] -> [(Int, Int)]
 go_down string_number list_of_points =
